b17d27c0b7edb5e89e8466958f0ff6222088e493
Cameron Angus <cjangus@gmail.com>
2019-11-13 23:58:47 +0000

    Attempted to disable pthreads for clang-cl

--- CMakeHelpers/functions.cmake
+++ CMakeHelpers/functions.cmake
@@ -5,8 +5,9 @@ if(POLICY CMP0054)
 endif()
 
 
+# @NOTE: Think this is causing failure when building with clang-cl, which identifies as Clang, but should use the MSVC runtime.
 function(use_pthread varName)
-	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU") # OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 		set(${varName} ${${varName}} pthread PARENT_SCOPE)
 	endif()
 endfunction()
